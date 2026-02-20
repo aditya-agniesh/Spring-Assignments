b9c966e98fc29e0f3c7fdcdd38b629d841e43503
aditya-agniesh <aditya.agniesh@tothenew.com>
2026-02-20 23:10:22 +0530

    jpa assignment 3

---
+++ spring-data-jpa-3/src/main/java/com/example/spring_data_jpa_3/SpringDataJpa3Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpa3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpa3Application.class, args);
+	}
+
+}
---
+++ spring-data-jpa-3/src/test/java/com/example/spring_data_jpa_3/SpringDataJpa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpa3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
